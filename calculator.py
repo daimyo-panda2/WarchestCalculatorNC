03735279eb5093433404303be2fd8ad383a7e32e
daimyo_panda2 <daimyo.taiki@gmail.com>
2025-11-04 18:53:51 +0900

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+print("-----Warchest Calculator By Fwoinist-----")
+CC = input("What is your city count? \n")
+CC = int(CC)  # Convert input to integer
+
+Alm = CC * 400
+Iro = CC * 250
+Gas = CC * 160
+Mun = CC * 175
+Foo = CC * 4000
+Mon = CC * 350000
+
+print("\n ---Your warchest is:---")
+
+print("Aluminum:", Alm)
+print("Iron:", Iro)
+print("Gasoline:", Gas)
+print("Munitions:", Mun)
+print("Food:", Foo)
+print("Money:", Mon)
